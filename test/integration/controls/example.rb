f17cd13acbaf37593ea1e50bd0985c24e20559d0
Sonu Saha <sonu.saha@progress.com>
2024-09-13 17:12:57 +0530

    spec: add inspec control to test mongodb resource as resource pack
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

---
+++ test/integration/controls/example.rb
@@ -0,0 +1,23 @@
+# NOTE: On CI/CD, we can possibly use the MongoDB Docker image to run this test or have some sort of mock for Mongo.
+# TODO: Discuss with the team about the strategy to run integration tests for MongoDB (or other resource packs.)
+
+control 'mongodb_conf' do
+  impact 1.0
+  title 'Verifies the configuration of MongoDB'
+  desc 'Verifies the configuration of MongoDB'
+
+  describe mongodb_conf("/opt/homebrew/etc/mongod.conf") do
+    its(["storage", "dbPath"]) { should eq "/opt/homebrew/var/mongodb" }
+    its(["net", "ipv6"]) { should eq true }
+  end
+end
+
+
+control 'mongodb_session' do
+  impact 1.0
+  title 'Verifies the session of MongoDB'
+  desc 'Verifies the session of MongoDB'
+  describe mongodb_session(user: "adminUser", password: "adminPassword", database: "admin").query(rolesInfo: "dbAdmin").params["roles"].first do
+    its(["role"]) { should eq "dbAdmin" }
+  end
+end
