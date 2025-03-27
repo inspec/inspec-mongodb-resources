e59d5bf1b37a3adf28b091fc2a7c00402749d55a
Sonu Saha <sonu.saha@progress.com>
2025-03-27 18:48:45 +0530

    chore: sync version with InSpec major version
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- lib/inspec-mongodb-resources/version.rb
+++ lib/inspec-mongodb-resources/version.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
 # This file simply makes it easier for CI engines to update
 # the version stamp, and provide a clean way for the gemspec
 # to learn the current version.
 module InspecPlugins
   module MongodbResources
-    VERSION = "0.1.8".freeze
+    VERSION = "7.0.0"
   end
 end
